fd9adced0760b75ad5e8c4358b869b77b464d5a8
Ronald <ronald_rozema@outlook.com>
2018-03-10 00:21:43 +0100

    archive page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog_index'),
     path('<int:pk>/', views.PostDetail.as_view(), name='blog_details'),
+    path('archive/<int:month>/<int:year>', views.ArchiveList.as_view(), name='archive_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import ListView, DetailView
+from django.db.models.functions import TruncMonth
+from django.db.models import Count
+from datetime import date
+
 from blog.models import Post
 
 
@@ -9,6 +13,12 @@ class PostList(ListView):
 
     def get_queryset(self):
         return Post.objects.filter(is_draft=False, is_removed=False, is_public=True).order_by('-publish_date')[:5]
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostList, self).get_context_data(*args, **kwargs)
+        context['popular_post_list'] = Post.objects.filter(is_draft=False, is_removed=False, is_public=True).order_by('-times_viewed')[:5]
+        context['archive_list'] = Post.objects.filter(is_draft=False, is_removed=False, is_public=True).annotate(month=TruncMonth('publish_date')).values('month').annotate(c=Count('id')).values('month', 'c')
+        return context
 
 
 class PostDetail(DetailView):
@@ -21,4 +31,21 @@ class PostDetail(DetailView):
         obj = self.get_object()
         obj.times_viewed = obj.times_viewed + 1
         obj.save()
+        return context
+
+
+class ArchiveList(ListView):
+
+    context_object_name = 'post_list'
+    template_name = 'blog/archive.html'
+    
+    def get_queryset(self):
+        month = self.kwargs['month']
+        year = self.kwargs['year']
+        return Post.objects.filter(is_draft=False, is_removed=False, is_public=True, publish_date__year=year, publish_date__month=month).order_by('publish_date')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArchiveList, self).get_context_data(*args, **kwargs)
+        context['archive_list'] = Post.objects.filter(is_draft=False, is_removed=False, is_public=True).annotate(month=TruncMonth('publish_date')).values('month').annotate(c=Count('id')).values('month', 'c')
+        context['date'] = date(year=self.kwargs['year'], month=self.kwargs['month'], day=1)
         return context
