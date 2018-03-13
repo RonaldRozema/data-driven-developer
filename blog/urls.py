541e85937bfa63352f713431870da86f5cce02bd
Ronald Rozema <ronald_rozema@outlook.com>
2018-03-13 16:27:42 +0100

    Initial blog app (#2)
    * detail pagina voor posts
    
    * installed zinnia blog
    
    * simple system for adding tags
    
    * added view count
    
    * only get posts that are public and not removed
    
    * archive page
    
    * minor changes
    
    * comment on blog post
    
    * anonymous user places comment

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.decorators.http import require_POST
 
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='index')
+    path('', views.PostList.as_view(), name='blog_index'),
+    path('<int:pk>/', views.PostDetail.as_view(), name='blog_details'),
+    path('archive/<int:month>/<int:year>/', views.ArchiveList.as_view(), name='archive_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import ListView
-from blog.models import Post
+from django.contrib.auth.models import User
+from django.db.models.functions import TruncMonth
+from django.db.models import Count
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, FormView, CreateView
+
+from datetime import date
+
+from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 
 class PostList(ListView):
@@ -8,4 +16,52 @@ class PostList(ListView):
     context_object_name = 'post_list'
 
     def get_queryset(self):
-        return Post.objects.order_by('-publish_date')[:5]
+        return Post.objects.filter(is_draft=False, is_removed=False, is_public=True).order_by('-publish_date')[:5]
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostList, self).get_context_data(*args, **kwargs)
+        context['popular_post_list'] = Post.objects.filter(is_draft=False, is_removed=False, is_public=True).order_by('-times_viewed')[:5]
+        context['archive_list'] = Post.objects.filter(is_draft=False, is_removed=False, is_public=True).annotate(month=TruncMonth('publish_date')).values('month').annotate(c=Count('id')).values('month', 'c')
+        return context
+
+
+class PostDetail(DetailView, FormView):
+
+    template_name = 'blog/details.html'
+    model = Post
+    form_class = CommentForm
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        obj = self.get_object()
+        obj.times_viewed = obj.times_viewed + 1
+        obj.save()
+        return context
+
+    def form_valid(self, form):
+        post = self.get_object()
+        comment = Comment(content=form.cleaned_data['content'], author=User.objects.get(username='anonymous'), post=post)
+        comment.save()
+        post.comment_set.add(comment)
+        post.save()
+        return super(PostDetail, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('blog_details', kwargs={'pk': self.get_object().pk})
+
+
+class ArchiveList(ListView):
+
+    context_object_name = 'post_list'
+    template_name = 'blog/archive.html'
+    
+    def get_queryset(self):
+        month = self.kwargs['month']
+        year = self.kwargs['year']
+        return Post.objects.filter(is_draft=False, is_removed=False, is_public=True, publish_date__year=year, publish_date__month=month).order_by('publish_date')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArchiveList, self).get_context_data(*args, **kwargs)
+        context['archive_list'] = Post.objects.filter(is_draft=False, is_removed=False, is_public=True).annotate(month=TruncMonth('publish_date')).values('month').annotate(c=Count('id')).values('month', 'c')
+        context['date'] = date(year=self.kwargs['year'], month=self.kwargs['month'], day=1)
+        return context
